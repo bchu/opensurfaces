eab07af06e3dfc0745cf55bc0dfeb16e2e8cfb7f
Brian Chu <bc@brianchu.com>
2016-06-10 21:23:42 -0700

    Add jupyter config files

--- post_install_all.sh
+++ post_install_all.sh
@@ -9,6 +9,8 @@ pip install jupyter
 
 jupyter-notebook --generate-config
 
-echo "Follow instructions at http://jupyter-notebook.readthedocs.io/en/latest/public_server.html to set up a jupyter server"
 # 'sha1:9dca29cc3390:799d0d366a84c22f5a065f49035aa2ba1c3e3851'
-
+echo "See instructions at http://jupyter-notebook.readthedocs.io/en/latest/public_server.html to set up a jupyter server"
+cp -f jupyter_notebook_config.py /home/vagrant/.jupyter/jupyter_notebook_config.py
+mkdir /home/vagrant/certs
+openssl req -x509 -nodes -days 365 -newkey rsa:1024 -keyout /home/vagrant/certs/mykey.key -out /home/vagrant/certs/mycert.pem
